d1518d063dc1d6076d97b1f4a7b0e34b8bedde3c
yaaskatt <yaaskatt@yandex.ru>
2019-05-19 22:08:45 +0300

    RPN

--- src/test/java/mirea/parser/ParserTest.java
+++ src/test/java/mirea/parser/ParserTest.java
@@ -24,68 +24,10 @@ public class ParserTest {
             System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
         }
         Parser parser = new Parser(tokenList);
-        parser.lang();
-        assertEquals(tokenList.size(), parser.num);
-    }
-
-    @Test
-    public void print_stmt() {
-        Lexer lexer = new Lexer(testFolder + "printTest");
-        List<Token> tokenList = lexer.getAllTokens();
-        assertFalse(tokenList.isEmpty());
-        for (int i=0; i<tokenList.size(); i++) {
-            System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
+        List<Element> out = (parser.lang());
+        assertEquals(tokenList.size()-1, parser.num);
+        for (int i=0; i<out.size(); i++) {
+            System.out.printf("%d: type: %s, value: %s\n", i, out.get(i).getType(), out.get(i).getValue());
         }
-        Parser parser = new Parser(tokenList);
-        assertTrue(parser.print_stmt());
-    }
-
-    @Test
-    public void objectOp_stmt() {
-        Lexer lexer = new Lexer(testFolder + "objectTest");
-        List<Token> tokenList = lexer.getAllTokens();
-        assertFalse(tokenList.isEmpty());
-        for (int i=0; i<tokenList.size(); i++) {
-            System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
-        }
-        Parser parser = new Parser(tokenList);
-        assertTrue(parser.objectOp_stmt());
-    }
-
-    @Test
-    public void assign_stmt() {
-        Lexer lexer = new Lexer(testFolder + "typesTest");
-        List<Token> tokenList = lexer.getAllTokens();
-        assertFalse(tokenList.isEmpty());
-        for (int i=0; i<tokenList.size(); i++) {
-            System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
-        }
-        Parser parser = new Parser(tokenList);
-        assertTrue(parser.assign_stmt());
-    }
-
-    @Test
-    public void declar_stmt() {
-        Lexer lexer = new Lexer(testFolder + "declarTest");
-        List<Token> tokenList = lexer.getAllTokens();
-        assertFalse(tokenList.isEmpty());
-        for (int i=0; i<tokenList.size(); i++) {
-            System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
-        }
-        Parser parser = new Parser(tokenList);
-        assertTrue(parser.declar_stmt());
-    }
-
-    @Test
-    public void if_stmt() {
-        Lexer lexer = new Lexer(testFolder + "ifTest");
-        List<Token> tokenList = lexer.getAllTokens();
-        for (int i=0; i<tokenList.size(); i++) {
-            System.out.printf("tokenType: %s, lexema: %s\n", tokenList.get(i).getTokenType(), tokenList.get(i).getLexema());
-        }
-        assertFalse(tokenList.isEmpty());
-        Parser parser = new Parser(tokenList);
-        assertTrue(parser.if_stmt());
-
     }
 }
